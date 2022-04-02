a46b84903ff54499a6f4275f1bd39559c5fd9e95
友岡湖太 <sirogamicota@gmail.com>
2022-04-02 20:02:05 +0900

    ff

--- frontend-diff-problems/src/App.test.tsx
+++ frontend-diff-problems/src/App.test.tsx
@@ -6,7 +6,7 @@ test("basic again", () => {
   expect(2).toBe(2);
 });
 
-test("renders learn react link", () => {
+test("renders learn react links", () => {
   render(<App />);
   const linkElement = screen.getByText(/Hello/i);
   expect(linkElement).toBeInTheDocument();
