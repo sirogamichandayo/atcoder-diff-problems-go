08aee3973c144af3e7ff416b38543e3c6718ead8
友岡湖太 <sirogamicota@gmail.com>
2022-04-17 20:53:22 +0900

    Update App.test.tsx

--- frontend-diff-problems/src/App.test.tsx
+++ frontend-diff-problems/src/App.test.tsx
@@ -1,15 +1,5 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { expect, test } from "@jest/globals";
 
 test("basic again", () => {
   expect(2).toBe(2);
 });
-
-/*
-test("renders learn react links", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello/i);
-  expect(linkElement).toBeInTheDocument();
-});
-*/
