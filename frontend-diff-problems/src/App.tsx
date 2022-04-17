67d1681cc86dad16703f548844b44a0b56bbb494
友岡湖太 <sirogamicota@gmail.com>
2022-04-17 18:31:41 +0900

    fix

--- frontend-diff-problems/src/App.tsx
+++ frontend-diff-problems/src/App.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import "./App.css";
 
 function App() {
-  const hello = 'hello';
 
   const a = 10;
   return (
     <div className="App">
       {a}
       <h1>Hello</h1>
-      <div>{hello}</div>
     </div>
   );
 }
