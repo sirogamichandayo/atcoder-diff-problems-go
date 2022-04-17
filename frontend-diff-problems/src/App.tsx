f29251823df0471ee061e66c239e9467f5bb7db9
友岡湖太 <sirogamicota@gmail.com>
2022-04-17 20:35:23 +0900

    a

--- frontend-diff-problems/src/App.tsx
+++ frontend-diff-problems/src/App.tsx
@@ -1,13 +1,37 @@
 import React from "react";
 import "./App.css";
+import { useQuery } from "react-query";
+import { apiClientV1 } from "./api/apiClient";
+
+type User = {
+  Id: number;
+  FirstName: string;
+  LastName: string;
+};
+type Users = User[];
 
 function App() {
-  const a = 10;
+  const { data, isLoading } = useQuery<Users>("get/users", async () => {
+    const { data } = await apiClientV1.get("/users");
+    return data;
+  });
 
   return (
     <div className="App">
-      {a}
       <h1>Hello</h1>
+      {isLoading ? (
+        "loading..."
+      ) : (
+        <ul>
+          {data?.map((d: User) => {
+            return (
+              <li key={d.Id}>
+                {d.Id}, {d.FirstName}, {d.LastName}
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 }
