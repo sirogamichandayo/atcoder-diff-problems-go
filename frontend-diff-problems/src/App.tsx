5501409864d87c4347c95def6fc8d7d334b65473
友岡湖太 <sirogamicota@gmail.com>
2022-04-17 18:24:48 +0900

    delete unsed

--- frontend-diff-problems/src/App.tsx
+++ frontend-diff-problems/src/App.tsx
@@ -1,28 +1,14 @@
-import React, { useState } from "react";
+import React from 'react';
 import "./App.css";
-import { request } from "graphql-request";
-
-const url = "https://localhost:8080/graphql";
-const query = `
-  query helloWorld {
-    helloWorld
-  }
-`;
 
 function App() {
-  const [hello, setHello] = useState("");
-
-  const graphQL = async () => {
-    const result = await request(url, query);
-    setHello(result.helloWorld);
-  };
+  const hello = 'hello';
 
   const a = 10;
   return (
     <div className="App">
       {a}
       <h1>Hello</h1>
-      <button onClick={graphQL}>Click</button>
       <div>{hello}</div>
     </div>
   );
