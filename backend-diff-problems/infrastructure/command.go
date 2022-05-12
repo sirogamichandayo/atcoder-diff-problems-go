16161b08598a9a823fea2663b15484312545ddfb
友岡湖太 <sirogamicota@gmail.com>
2022-05-12 11:41:17 +0900

    途中

--- backend-diff-problems/infrastructure/command.go
+++ backend-diff-problems/infrastructure/command.go
@@ -30,6 +30,57 @@ var updateProblemDifficultiesCmd = &cobra.Command{
 	},
 }
 
+<<<<<<< Updated upstream
+=======
+var updateAllUserFirstAcSubmissionCmd = &cobra.Command{
+	Use:   "update-all-ac-submission",
+	Short: "全てのatcoderの最初のAC提出を更新",
+	Long:  "atcoder problemsのapiを叩いて、得られた情報を元にDBに保存します",
+	RunE: func(cmd *cobra.Command, args []string) (err error) {
+		config, err := conf.LoadConfig()
+		if err != nil {
+			return
+		}
+
+		command := commands.NewUpdateUserFirstAcSubmissionCommand(
+			NewSqlHandler(config.SinDb),
+			NewRequestHandler(),
+		)
+		err = command.UpdateAll()
+		return
+	},
+}
+
+var startApiCmd = &cobra.Command{
+	Use:   "api",
+	Short: "apiを起動",
+	Long:  "atcoder diff problemsのapiを起動します",
+	RunE: func(cmd *cobra.Command, args []string) (err error) {
+		RouterInitialize()
+		return Router.Run()
+	},
+}
+
+var updateUserFirstAcSubmissionCmd = &cobra.Command{
+	Use:   "update-ac-submission",
+	Short: "すでに更新済みの部分からatcoderの最初のAC提出を更新",
+	Long:  "atcoder problemsのapiを叩いて、得られた情報を元にDBに保存します",
+	RunE: func(cmd *cobra.Command, args []string) (err error) {
+		config, err := conf.LoadConfig()
+		if err != nil {
+			return
+		}
+
+		command := commands.NewUpdateUserFirstAcSubmissionCommand(
+			NewSqlHandler(config.SinDb),
+			NewRequestHandler(),
+		)
+		err = command.UpdateFromUpdatedAt()
+		return
+	},
+}
+
+>>>>>>> Stashed changes
 func Execute() {
 	rootCmd.AddCommand(updateProblemDifficultiesCmd)
 	if err := rootCmd.Execute(); err != nil {
