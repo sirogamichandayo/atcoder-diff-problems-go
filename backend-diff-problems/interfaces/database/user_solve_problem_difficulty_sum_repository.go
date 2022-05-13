854f776d1a173329cbfaf3d64959e38171a69566
友岡湖太 <sirogamicota@gmail.com>
2022-05-14 02:42:18 +0900

    sql書いた

--- backend-diff-problems/interfaces/database/user_solve_problem_difficulty_sum_repository.go
+++ backend-diff-problems/interfaces/database/user_solve_problem_difficulty_sum_repository.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"diff-problems/domain/entity"
+	"fmt"
 )
 
 type UserSolveProblemDifficultySumRepository struct {
@@ -9,6 +10,15 @@ type UserSolveProblemDifficultySumRepository struct {
 }
 
 func (u UserSolveProblemDifficultySumRepository) Upsert(list entity.UserSolveProblemDifficultySumList) error {
-	//TODO implement me
-	panic("implement me")
+	placeholders, values := list.MakeValueForUpsertMySql()
+	query := fmt.Sprintf(`
+INSERT INTO user_solve_problem_difficulty_sum (user_id, clip_difficulty_sum)
+VALUES %s
+ON DUPLICATE KEY UPDATE
+  user_id = VALUES(user_id)
+`,
+		placeholders)
+	_, err := u.Execute(query, values)
+
+	return err
 }
