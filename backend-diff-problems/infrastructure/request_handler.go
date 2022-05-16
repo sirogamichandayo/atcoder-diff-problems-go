03fee81a86f7777bf4d9b83b926ac7db44dbe0b6
友岡湖太 <sirogamicota@gmail.com>
2022-05-16 17:04:11 +0900

    request handlerの作成

--- backend-diff-problems/infrastructure/request_handler.go
+++ backend-diff-problems/infrastructure/request_handler.go
@@ -2,27 +2,25 @@ package infrastructure
 
 import (
 	"bytes"
-	"compress/gzip"
 	"diff-problems/interfaces/api"
 	"net/http"
 )
 
-type GzipRequestHandler struct {
+type RequestHandler struct {
 }
 
-func NewGzipRequestHandler() api.RequestHandler {
-	clientHandler := new(GzipRequestHandler)
+func NewRequestHandler() api.RequestHandler {
+	clientHandler := new(RequestHandler)
 
 	return clientHandler
 }
 
-func (handler *GzipRequestHandler) Get(url string, headers map[string]string) (api.Response, error) {
+func (handler *RequestHandler) Get(url string, headers map[string]string) (api.Response, error) {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
 	}
 	req.Close = true
-	req.Header.Set("Accept-Encoding", "gzip")
 	for key, value := range headers {
 		req.Header.Set(key, value)
 	}
@@ -34,13 +32,8 @@ func (handler *GzipRequestHandler) Get(url string, headers map[string]string) (a
 
 	defer res.Body.Close()
 
-	reader, err := gzip.NewReader(res.Body)
-	if err != nil {
-		return nil, err
-	}
-
 	output := bytes.Buffer{}
-	if _, err := output.ReadFrom(reader); err != nil {
+	if _, err := output.ReadFrom(res.Body); err != nil {
 		return nil, err
 	}
 	return Response{res.StatusCode, output.Bytes()}, err
