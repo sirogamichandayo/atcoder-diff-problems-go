313310753a4ea34292afe1ee35990b5e882a694e
友岡湖太 <sirogamicota@gmail.com>
2022-05-16 19:21:08 +0900

    client作成

---
+++ backend-diff-problems/interfaces/api/converter/content_result_list.go
@@ -0,0 +1,41 @@
+package converter
+
+import (
+	"diff-problems/domain/vo"
+	"diff-problems/util_tool"
+	"encoding/json"
+	"fmt"
+)
+
+func ConvertContentResultList(jsonBytes []byte) (vo.ContestResultList, error) {
+	var rawList []map[string]interface{}
+	err := json.Unmarshal(jsonBytes, &rawList)
+	if err != nil {
+		return nil, err
+	}
+
+	list := make(vo.ContestResultList, 0, len(rawList))
+	for _, raw := range rawList {
+		isRated, hasKey := raw["IsRated"].(bool)
+		if !hasKey {
+			return nil, fmt.Errorf("not exist isRated")
+		}
+		rawNewRating, hasKey := raw["NewRating"]
+		if !hasKey {
+			return nil, fmt.Errorf("not exist newRating")
+		}
+		newRating := int(rawNewRating.(float64))
+		rawEndTime, hasKey := raw["EndTime"].(string)
+		if !hasKey {
+			return nil, fmt.Errorf("not exist endTime")
+		}
+		endTime, err := util_tool.ConvertIso8601JstToEpochTime(rawEndTime)
+		if err != nil {
+			return nil, err
+		}
+
+		list = append(list, vo.ContestResult{IsRated: isRated, NewRating: newRating, EndTime: endTime})
+	}
+
+	return list, nil
+}
---
+++ backend-diff-problems/interfaces/api/converter/content_result_list_test.go
@@ -0,0 +1,24 @@
+package converter
+
+import (
+	"diff-problems/domain/vo"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_ConvertContentResultList_正常系(t *testing.T) {
+	bytes := []byte(`[{"IsRated":true,"Place":59,"OldRating":0,"NewRating":1255,"Performance":2455,"InnerPerformance":2455,"ContestScreenName":"arc061.contest.atcoder.jp","ContestName":"AtCoder Regular Contest 061","ContestNameEn":"","EndTime":"2016-09-11T22:40:00+09:00"},{"IsRated":true,"Place":9,"OldRating":1255,"NewRating":2155,"Performance":3192,"InnerPerformance":3192,"ContestScreenName":"arc064.contest.atcoder.jp","ContestName":"AtCoder Regular Contest 064","ContestNameEn":"","EndTime":"2016-12-04T22:40:00+09:00"},{"IsRated":true,"Place":8,"OldRating":2155,"NewRating":2676,"Performance":3623,"InnerPerformance":3623,"ContestScreenName":"agc010.contest.atcoder.jp","ContestName":"AtCoder Grand Contest 010","ContestNameEn":"","EndTime":"2017-02-04T22:50:00+09:00"},{"IsRated":true,"Place":41,"OldRating":2676,"NewRating":2728,"Performance":2974,"InnerPerformance":2974,"ContestScreenName":"agc014.contest.atcoder.jp","ContestName":"AtCoder Grand Contest 014","ContestNameEn":"","EndTime":"2017-05-06T23:10:00+09:00"},{"IsRated":true,"Place":45,"OldRating":2728,"NewRating":2733,"Performance":2813,"InnerPerformance":2813,"ContestScreenName":"agc015.contest.atcoder.jp","ContestName":"AtCoder Grand Contest 015","ContestNameEn":"","EndTime":"2017-05-27T22:50:00+09:00"}]`)
+	actual, err := ConvertContentResultList(bytes)
+	assert.Nil(t, err)
+
+	expected := vo.ContestResultList{
+		{true, 1255, 1473601200},
+		{true, 2155, 1480858800},
+		{true, 2676, 1486216200},
+		{true, 2728, 1494079800},
+		{true, 2733, 1495893000},
+	}
+
+	assert.Equal(t, expected, actual)
+}
