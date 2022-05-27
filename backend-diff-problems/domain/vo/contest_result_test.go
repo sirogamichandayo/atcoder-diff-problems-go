aac682aa7107d5f1135cf64a4c70d135a35476e4
友岡湖太 <sirogamicota@gmail.com>
2022-05-27 23:02:06 +0900

    ratingテスト

--- backend-diff-problems/domain/vo/contest_result_test.go
+++ backend-diff-problems/domain/vo/contest_result_test.go
@@ -7,17 +7,18 @@ import (
 )
 
 func Test_Last_正常系(t *testing.T) {
+	a, b, c := 100, 200, 300
 	list := ContestResultList{
 		{true, Rating{
-			rating: 100,
+			rating: &a,
 			color:  Gray,
 		}, 1000},
 		{true, Rating{
-			rating: 200,
+			rating: &b,
 			color:  Gray,
 		}, 2000},
 		{true, Rating{
-			rating: 300,
+			rating: &c,
 			color:  Gray,
 		}, 3000},
 	}
@@ -25,22 +26,23 @@ func Test_Last_正常系(t *testing.T) {
 	actual, err := list.Last()
 	assert.Nil(t, err)
 
-	expected := ContestResult{true, Rating{rating: 300, color: Gray}, 3000}
+	expected := ContestResult{true, Rating{rating: &c, color: Gray}, 3000}
 	assert.Equal(t, expected, actual)
 }
 
 func Test_ExactByRated_正常系(t *testing.T) {
+	a, b, c := 100, 200, 300
 	list := ContestResultList{
-		{true, Rating{rating: 100, color: Gray}, 1000},
-		{true, Rating{rating: 200, color: Gray}, 2000},
-		{false, Rating{rating: 300, color: Gray}, 3000},
+		{true, Rating{rating: &a, color: Gray}, 1000},
+		{true, Rating{rating: &b, color: Gray}, 2000},
+		{false, Rating{rating: &c, color: Gray}, 3000},
 	}
 
 	actual := list.ExactByRated()
 
 	expected := ContestResultList{
-		{true, Rating{rating: 100, color: Gray}, 1000},
-		{true, Rating{rating: 200, color: Gray}, 2000},
+		{true, Rating{rating: &a, color: Gray}, 1000},
+		{true, Rating{rating: &b, color: Gray}, 2000},
 	}
 	assert.Equal(t, expected, actual)
 }
