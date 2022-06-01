6e359178f372a1166229ee38283ffabe49243ddb
友岡湖太 <sirogamicota@gmail.com>
2022-06-01 23:51:48 +0900

    fix test

--- backend-diff-problems/interfaces/controllers_test/rank_controller_test.go
+++ backend-diff-problems/interfaces/controllers_test/rank_controller_test.go
@@ -18,8 +18,8 @@ func Test_RankController_Show_正常系(t *testing.T) {
 INSERT INTO user_solve_problem_difficulty_sum
 VALUES 
 ("test1", 1000.0, 1),("test2", 900.0, 2),("test3", 700.0, 3),
-("test4", 600.0, 4),("test5", 500.0, 5),("test6", 400.0, 6),
-("test7", 300.0, 7),("test8", 200.0, 2) 
+("test4", 600.0, 4) ,("test5", 500.0, 5),("test6", 400.0, 6),
+("test7", 300.0, 7) ,("test8", 200.0, 2) 
 `)
 	assert.Nil(t, err)
 
@@ -32,7 +32,10 @@ VALUES
 	controller.Show(c)
 
 	assert.Equal(t,
-		`[{"clipDifficultySum":900,"rank":2,"userId":"test2"},{"clipDifficultySum":200,"rank":2,"userId":"test8"},{"clipDifficultySum":700,"rank":3,"userId":"test3"}]`,
+		`
+[{"clipDifficultySum":900,"rank":2,"userId":"test2"},
+{"clipDifficultySum":200,"rank":2,"userId":"test8"},
+{"clipDifficultySum":700,"rank":3,"userId":"test3"}]`,
 		w.Body.String(),
 	)
 }
